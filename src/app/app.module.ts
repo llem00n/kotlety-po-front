c43efc136e6a180682fe83a3db9b9bc6a5abaf60
Dmytro <reserv2017@ukr.net>
2023-05-15 22:21:20 +0300

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    BrowserModule,
+    HeaderComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
